5c8f3c05e1e816bfcc7e1e0f461763b47ecd44fd
Denys Bilonozhko <deformix@gmail.com>
2021-12-23 22:47:44 +0200

    feat: add effects on scroll

--- app/js/main.js
+++ app/js/main.js
@@ -2,9 +2,39 @@ var splide = new Splide('.splide', {
   perPage: 4,
   perMove: 1,
   rewind: true,
-  autoWidth: true,
+
   gap: 30,
-  arrow : 'slider__arrow',
+  arrow: 'slider__arrow',
 });
 
 splide.mount();
+
+document.addEventListener('scroll', onScroll);
+
+function onScroll(e) {
+  const currentPosition = window.scrollY;
+  const links = document.querySelectorAll('.header__nav-link');
+  const sections = document.querySelectorAll('section');
+  const header = document.querySelector('.header');
+
+  if (currentPosition > 200) {
+    header.classList.add('black');
+  } else {
+    header.classList.remove('black');
+  }
+
+  sections.forEach(function (el) {
+    if (
+      el.offsetTop <= currentPosition &&
+      el.offsetTop + el.offsetHeight > currentPosition
+    ) {
+      links.forEach(function (link) {
+        link.classList.remove('header__nav-link--active');
+
+        if (el.getAttribute('id') === link.getAttribute('href').substring(1)) {
+          link.classList.add('header__nav-link--active');
+        }
+      });
+    }
+  });
+}
